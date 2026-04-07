e87da373f9158668a756552eeca1884c88c08237
Loran Mariia <mariia.loran.25@.kse.org.ua>
2026-04-07 18:28:41 +0300

    Data

--- Week10/Week10/Action.cs
+++ Week10/Week10/Action.cs
@@ -2,5 +2,17 @@
 
 public class Action
 {
+    public int NumOfMove { get; set; }
+    public string Description { get; set; }
+    public string TypeOfAction {get;set;}
+    public string ChangeCharactiristics {get;set;}
+
+    public Action(int numOfMove, string description, string typeOfAction, string changeCharactiristics)
+    {
+        NumOfMove = numOfMove;
+        Description = description;
+        TypeOfAction = typeOfAction;
+        ChangeCharactiristics = changeCharactiristics;
+    }
     
 }
--- Week10/Week10/Player.cs
+++ Week10/Week10/Player.cs
@@ -2,5 +2,19 @@
 
 public class Player
 {
-    
+    public string Name { get; set; }
+    public string Role { get; set; }
+    public int Level { get; set; }
+    public int CountOFHealth { get; set; }
+    public string ConditionOfPlayer { get; set; }
+
+    public Player(string name, string role, int level, int countOfHealth, string conditionOfPlayer)
+    {
+        Name = name;
+        Role = role;
+        Level = level;
+        CountOFHealth = countOfHealth;
+        ConditionOfPlayer = conditionOfPlayer;
+        
+    }
 }
